0aa6e8fda1e00e18bde116506c8b4cd9d1f08ee7
Damiaou <damsauvagere@gmail.com>
2021-11-26 18:47:57 +0100

    feat: slider + buutton

--- src/App.jsx
+++ src/App.jsx
@@ -1,34 +1,89 @@
-import { useState } from "react";
-import logo from "./logo.svg";
-import "./App.css";
 import create from "zustand";
+import { Button } from "@chakra-ui/button";
+import {
+  Container,
+  Center,
+  Heading,
+  VStack,
+  Badge,
+  HStack,
+} from "@chakra-ui/react";
+import {
+  Slider,
+  SliderTrack,
+  SliderFilledTrack,
+  SliderThumb,
+} from "@chakra-ui/react";
 
 const useStore = create((set) => ({
   count: 0,
   increaseCount: () => set((state) => ({ count: state.count + 1 })),
+  decreaseCount: () => set((state) => ({ count: state.count - 1 })),
   resetCount: () => set({ count: 0 }),
+  setCount: (value) => set({ count: value }),
 }));
+
 function App() {
   const count = useStore((state) => state.count);
 
+  const setCount = (value) => {
+    useStore.setState({ count: value });
+  };
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>Hello Vite + React!</p>
-        <p>
-          <button
-            type="button"
-            onClick={useStore((state) => state.increaseCount)}
+    <Container sx={{ minHeight: "100%" }}>
+      <Center>
+        <VStack spacing="2em">
+          <Heading sx={{ color: "orange" }}>Vite + Zustand + Chakra UI</Heading>
+          <HStack>
+            <Button
+              onClick={useStore((state) => state.decreaseCount)}
+              size="lg"
+              colorScheme="teal"
+              width="100px"
+            >
+              Minus!
+            </Button>
+            <Button
+              width="100px"
+              size="lg"
+              onClick={useStore((state) => state.resetCount)}
+            >
+              Reset!
+            </Button>
+            <Button
+              onClick={useStore((state) => state.increaseCount)}
+              size="lg"
+              colorScheme="teal"
+              width="100px"
+            >
+              Plus!
+            </Button>
+          </HStack>
+          <Badge
+            sx={{ padding: "0.5em", paddingX: "1em" }}
+            variant="outline"
+            colorScheme="purple"
           >
-            count is: {count}
-          </button>
-        </p>
-        <button type="button" onClick={useStore((state) => state.resetCount)}>
-          Reset
-        </button>
-      </header>
-    </div>
+            {count}
+          </Badge>
+          <Slider
+            colorScheme="teal"
+            min={-100}
+            max={100}
+            step={1}
+            onChange={(val) => setCount(val)}
+            defaultValue={0}
+            // onChangeEnd={(val) => setCount(val)}
+          >
+            <SliderTrack>
+              <SliderFilledTrack />
+            </SliderTrack>
+            <SliderThumb />
+          </Slider>
+        </VStack>
+      </Center>
+    </Container>
   );
 }
 
--- src/main.jsx
+++ src/main.jsx
@@ -1,11 +1,15 @@
-import React from 'react'
-import ReactDOM from 'react-dom'
-import './index.css'
-import App from './App'
+import React from "react";
+import ReactDOM from "react-dom";
+// import "./index.css";
+import App from "./App";
+
+import { ChakraProvider } from "@chakra-ui/react";
 
 ReactDOM.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>,
-  document.getElementById('root')
-)
+  <ChakraProvider>
+    <React.StrictMode>
+      <App />
+    </React.StrictMode>
+  </ChakraProvider>,
+  document.getElementById("root")
+);
